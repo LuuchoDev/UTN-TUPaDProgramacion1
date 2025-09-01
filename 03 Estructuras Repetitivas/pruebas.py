69f0c392e883733e45b6a9fead54617bf443752a
Klow45 <emanuelsosa00@hotmail.com>
2025-09-01 18:03:07 -0300

    Update pruebas.py

--- 03 Estructuras Repetitivas/pruebas.py
+++ 03 Estructuras Repetitivas/pruebas.py
@@ -1,1 +1,9 @@
-0
+
+
+num = int(input("Ingrese un número: "))
+numInvertido = 0
+while num > 0:
+    digito = num % 10
+    numInvertido = numInvertido * 10 + digito
+    num = num // 10
+print(f"El número invertido es: {numInvertido}")
