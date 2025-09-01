e30b2f825992a6490758910ef505f9f5bf6c59c4
Klow45 <emanuelsosa00@hotmail.com>
2025-09-01 16:15:42 -0300

    Add exercises for repetitive structures in Python

--- 03 Estructuras Repetitivas/pruebas.py
+++ 03 Estructuras Repetitivas/pruebas.py
@@ -1,2 +1,13 @@
-for x in range(1, 11, 2):
-    print(x, "Debo aprender ciclos")
+num1 = int(input("Ingrese un número: "))
+num2 = int(input("Ingrese otro número: "))
+
+## Determino el menor y el mayor
+menor = min(num1, num2)
+mayor = max(num1, num2)
+
+## Calculo la suma de los números entre el menor y el mayor
+suma = 0
+for i in range (menor + 1 , mayor):
+    suma += i
+print(f"La suma de los números entre {menor} y {mayor} es: {suma}")
+
---
+++ 03 Estructuras Repetitivas/tpEstructurasRepetitivas.py
@@ -0,0 +1,17 @@
+## Ejercicio 1
+
+for i in range(101):
+    print(i)
+    
+## Ejercicio 2
+
+num = int(input("Ingrese un número: "))
+
+## Convierto el numero a string y cuento la cantidad de digitos
+cantDigitos = len(str(num))
+
+## Imprimo la cantidad de dígitos                         
+print("La cantidad de dígitos es:", cantDigitos)    
+
+## Ejercicio 3
+
