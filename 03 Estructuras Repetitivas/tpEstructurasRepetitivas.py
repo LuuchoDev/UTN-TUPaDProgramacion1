e2e17bdd9967f0ba7f33a3283a6988c9d4b0a0e9
Klow45 <emanuelsosa00@hotmail.com>
2025-09-01 16:16:26 -0300

    Add solution for sum between two numbers

--- 03 Estructuras Repetitivas/tpEstructurasRepetitivas.py
+++ 03 Estructuras Repetitivas/tpEstructurasRepetitivas.py
@@ -15,3 +15,18 @@ print("La cantidad de dígitos es:", cantDigitos)
 
 ## Ejercicio 3
 
+num1 = int(input("Ingrese un número: "))
+num2 = int(input("Ingrese otro número: "))
+
+## Determino el menor y el mayor
+menor = min(num1, num2)
+mayor = max(num1, num2)
+
+## Calculo la suma de los números entre el menor y el mayor
+suma = 0
+for i in range (menor + 1 , mayor):
+    suma += i
+print(f"La suma de los números entre {menor} y {mayor} es: {suma}")
+
+## Ejercicio 4
+
