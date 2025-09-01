dda322c9277f1757cfb62c734655c76e0a06f27a
Klow45 <emanuelsosa00@hotmail.com>
2025-08-31 22:27:03 -0300

    Update tpEstructurasCondicionales.py

--- Estructuras Condicionales/tpEstructurasCondicionales.py
+++ Estructuras Condicionales/tpEstructurasCondicionales.py
@@ -1,9 +1,34 @@
-## Ejercicio1
+## Ejercicio 1
 edad = int(input("Ingrese su edad: "))
 if edad >= 18:
     print("Eres mayor de edad.")
 else:
     print("Eres menor de edad.")
 
-## Ejercicio2
+## Ejercicio 2
+nota = float(input("Ingrese su nota: "))
+if nota >= 6:
+    print("Aprobado")
+else:
+    print("Desaprobado")
+    
+## Ejercicio 3
+num =int(input("Ingrese un número: "))
+if num % 2 == 0:
+    print("El número es par.")
+else:
+    print("Por Favor, ingrese un número par.")
 
+## Ejercicio 4
+edad = int(input("Ingrese su edad: "))
+
+if edad < 12:
+    print("Eres un niño.")
+elif (edad >= 12) and (edad < 18):
+    print("Eres un adolescente.")
+elif (edad >= 18) and (edad < 30):
+    print("Eres un adulto joven.")
+elif (edad >= 30):
+    print("Eres un adulto.")
+
+## Ejercicio 5
