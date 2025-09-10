945461367255ba6404dd74283f45b3e42f0eeee4
Klow45 <emanuelsosa00@hotmail.com>
2025-09-10 12:16:40 -0300

    Remove Pruebas.py and add product list exercise
    Deleted Pruebas.py and added a new exercise to tpListas.py for managing a list of products, including adding, sorting, and removing products based on user input.

--- 04 Listas/tpListas.py
+++ 04 Listas/tpListas.py
@@ -11,3 +11,39 @@ print(f"La nota mas alta es: {max(notas)}")
 print(f"La nota mas baja es: {min(notas)}")
 
 ## Ejercicio 2
+
+## Crea una lista vacía para almacenar productos
+productos = []
+print("Ingrese los productos que desea agregar: ")
+## Solicita al usuario que ingrese 5 productos
+for i in range(5):
+    producto = input(f"Producto {i+1}: ")
+    productos.append(producto)
+
+## Muestra la lista en orden alfabético
+productos_ordenados = sorted(productos)
+print("Lista de productos en orden alfabético:")
+for p in productos_ordenados:
+    print(f"- {p}")
+
+## Consulta al usuario si desea eliminar un producto
+EliminarSioNo = input("¿Desea eliminar algún producto de la lista? (s/n): ")
+if EliminarSioNo in 'sS':
+    producto_a_eliminar = input("Ingrese el nombre del producto que desea eliminar: ")
+    ## verifica si el producto está en la lista antes de eliminarlo
+    if producto_a_eliminar in productos:
+        productos.remove(producto_a_eliminar)
+        print(f"Producto '{producto_a_eliminar}' eliminado.")
+        ## Muestra la lista actualizada luego de eliminar el producto
+        print("Lista actualizada de productos:")
+        for p in productos:
+            print(f"- {p}")
+    else:
+        print(f"Producto '{producto_a_eliminar}' no encontrado en la lista.")
+else:
+    ## Muestra la lista actualizada
+    print("Lista actualizada de productos:")
+    for p in productos:
+        print(f"- {p}")
+        
+## Ejercicio 3
