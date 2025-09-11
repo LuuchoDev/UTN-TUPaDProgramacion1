d60828ad2ab196b0d4afefad87bc4f831b2d888f
Klow45 <emanuelsosa00@hotmail.com>
2025-09-11 18:25:21 -0300

    Update Pruebas.py

--- 04 Listas/Pruebas.py
+++ 04 Listas/Pruebas.py
@@ -1,22 +1,17 @@
-## Lista de alumnos
-alumnos = ["Ana", "Luis", "Carlos", "Marta", "Lucía", "Jorge", "Sofía", "Diego"]
-print(alumnos)
+temperaturas = [
+    [10, 20],
+    [12, 22],
+    [8, 18],
+    [11, 21],
+    [9, 19],
+    [13, 23],
+    [7, 17]
+]
+## calcular el promedio de las minimas y maximas
+minimas = [fila[0] for fila in temperaturas]
+maximas = [fila[1] for fila in temperaturas]
 
-## Consulta al usuario si desea agregar un nuevo alumno o eliminar uno existente.
-Agregar_O_Eliminar = input("¿Desea agregar un nuevo alumno (a) o eliminar uno existente (e)? (a/e): ")
-if Agregar_O_Eliminar in 'aA':
-    ## Solicita el nombre del nuevo alumno
-    nuevo_alumno = input("Ingrese el nombre del nuevo alumno: ")
-    alumnos.append(nuevo_alumno)
-    print(f"Alumno '{nuevo_alumno}' agregado.")
-elif Agregar_O_Eliminar in 'eE':
-    ## Solicita el nombre del alumno a eliminar
-    alumno_a_eliminar = input("Ingrese el nombre del alumno que desea eliminar: ")
-    ## Verifica si el alumno está en la lista antes de eliminarlo
-    if alumno_a_eliminar in alumnos:
-        alumnos.remove(alumno_a_eliminar)
-        print(f"Alumno '{alumno_a_eliminar}' eliminado.")
-    else:
-        print(f"Alumno '{alumno_a_eliminar}' no encontrado en la lista.")
-
-print(f"La lista actualizada es: {alumnos}")
+promedio_minimas = sum(minimas) / len(minimas)
+promedio_maximas = sum(maximas) / len(maximas)
+print(f'Promedio de temperaturas mínimas: {promedio_minimas}')
+print(f'Promedio de temperaturas máximas: {promedio_maximas}')
