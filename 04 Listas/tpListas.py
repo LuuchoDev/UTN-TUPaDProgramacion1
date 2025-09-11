b9311b5db70f8a49e222cb0520271e61be1f4601
Klow45 <emanuelsosa00@hotmail.com>
2025-09-11 18:50:35 -0300

    actualizacion de ejercicios

--- 04 Listas/tpListas.py
+++ 04 Listas/tpListas.py
@@ -118,3 +118,44 @@ numeros = [numeros[-1]] + numeros[:-1]
 print(f"Lista rotada: {numeros}")
 
 ## Ejercicio 7
+
+temperaturas = [
+    [10, 20],
+    [12, 22],
+    [8, 18],
+    [11, 21],
+    [9, 19],
+    [13, 23],
+    [7, 17]
+]
+## calcular el promedio de las minimas y maximas
+minimas = [fila[0] for fila in temperaturas]
+maximas = [fila[1] for fila in temperaturas]
+
+promedio_minimas = sum(minimas) / len(minimas)
+promedio_maximas = sum(maximas) / len(maximas)
+print(f'Promedio de temperaturas mínimas: {promedio_minimas}')
+print(f'Promedio de temperaturas máximas: {promedio_maximas}')
+
+## Ejercicio 8
+
+notas = [
+    [8, 7, 9],   # Estudiante 1
+    [6, 5, 7],   # Estudiante 2
+    [9, 8, 10],  # Estudiante 3
+    [7, 6, 8],   # Estudiante 4
+    [10, 9, 9]   # Estudiante 5
+]
+
+## Calcula y muestra el promedio de cada estudiante
+for i in range(len(notas)):
+    promedio = sum(notas[i]) / len(notas[i])
+    print(f"El promedio del estudiante {i+1} es: {promedio}.")
+    
+## Calcula y muestra el promedio de cada materia
+for j in range(3):
+    suma = 0
+    for i in range(len(notas)):
+        suma += notas[i][j]
+    promedio = suma / len(notas)
+    print(f"El promedio de la materia {j+1} es: {promedio:.2f}.")
