e22be8db92084635de6fe63f19c0e4966ab975bb
LuuchoDev <emanuelsosa00@hotmail.com>
2025-10-06 21:29:41 -0300

    add exercise 8

--- 05 Funciones/Pruebas.py
+++ 05 Funciones/Pruebas.py
@@ -1,12 +1,7 @@
-def operaciones_basicas(a, b):
-    ## Realiza las operaciones básicas entre dos números y devuelve los resultados.
-    suma = a + b
-    resta = a - b
-    multiplicacion = a * b
-    division = a / b
-    return (suma, resta, multiplicacion, division)
-
-num1 = int(input("Ingrese el primer número: "))
-num2 = int(input("Ingrese el segundo número: "))
-resultados = operaciones_basicas(num1, num2)
-print(f"Suma: {resultados[0]}\nResta: {resultados[1]}\nMultiplicación: {resultados[2]}\nDivisión: {resultados[3]}")
+def calcular_imc(peso, altura):
+    ## Calcula el índice de masa corporal (IMC) dado el peso y la altura.
+    imc = peso / altura
+    return imc
+peso = float(input("Ingrese su peso: "))
+altura = float(input("Ingrese su altura: "))
+print(f"su indice de masa corporal es: {calcular_imc(peso, altura),}")
--- 05 Funciones/tpFunciones.py
+++ 05 Funciones/tpFunciones.py
@@ -86,3 +86,16 @@ num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
 resultados = operaciones_basicas(num1, num2)
 print(f"Suma: {resultados[0]}\nResta: {resultados[1]}\nMultiplicación: {resultados[2]}\nDivisión: {resultados[3]}")
+
+# ==========================================
+#               EJERCICIO 8
+# ==========================================
+
+def calcular_imc(peso, altura):
+    ## Calcula el índice de masa corporal (IMC) dado el peso y la altura.
+    imc = peso / altura
+    return imc
+
+peso = float(input("Ingrese su peso: "))
+altura = float(input("Ingrese su altura: "))
+print(f"su indice de masa corporal es: {calcular_imc(peso, altura),}")
