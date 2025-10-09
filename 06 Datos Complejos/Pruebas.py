ab7d35e62484133ffce4529088fe70cb903f1b7b
LuuchoDev <emanuelsosa00@hotmail.com>
2025-10-09 14:04:15 -0300

    Update Pruebas.py

---
+++ 06 Datos Complejos/Pruebas.py
@@ -0,0 +1,7 @@
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
+## Agrega nuevas frutas y sus precios
+precios_frutas['Naranja'] = 1200
+precios_frutas["manzana"] = 1500
+precios_frutas["Pera"] = 2300
+print(precios_frutas)
+
