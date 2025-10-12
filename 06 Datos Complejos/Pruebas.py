6d441dd3bc11e7864d4264560633e5153dedc5b1
LuuchoDev <emanuelsosa00@hotmail.com>
2025-10-12 01:08:21 -0300

    Update Pruebas.py

--- 06 Datos Complejos/Pruebas.py
+++ 06 Datos Complejos/Pruebas.py
@@ -1,14 +1,14 @@
-precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
-## Agrega nuevas frutas y sus precios
-precios_frutas['Naranja'] = 1200
-precios_frutas["Manzana"] = 1500
-precios_frutas["Pera"] = 2300
-print(precios_frutas)
-## Actualiza los precios de las frutas 
-precios_frutas["Banana"] = 1330
-precios_frutas["Manzana"] = 1700
-precios_frutas["Melón"] = 2800
-print(precios_frutas)
+contactos = {}
+for i in range(1):
+    nombre = input("Ingrese el nombre del contacto: ")
+    telefono = input(f"Ingrese el teléfono del contacto {nombre}: ")
+    contactos[nombre] = telefono
+    print(f"Contacto agregado: {nombre} - {telefono}")
 
-lista_frutas = precios_frutas.keys()
-print(lista_frutas)
+print("\nLista de contactos: ")
+lista_contactos = contactos.keys()
+print(lista_contactos)
+print("\nBúsqueda de contactos:")
+nombre_buscar = input("Ingrese el nombre del contacto a buscar: ")
+if nombre_buscar in contactos:
+    print(f"El contacto {nombre_buscar} tiene el numero {telefono}")
