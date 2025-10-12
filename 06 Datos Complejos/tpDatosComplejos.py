29509fccc59996a4abb9b2e8163ee99df694fffe
LuuchoDev <emanuelsosa00@hotmail.com>
2025-10-12 01:29:04 -0300

    add exersice 4

--- 06 Datos Complejos/tpDatosComplejos.py
+++ 06 Datos Complejos/tpDatosComplejos.py
@@ -45,3 +45,23 @@ print(lista_frutas)
 # ==========================================
 #               EJERCICIO 4
 # ==========================================
+contactos = {}
+for i in range(5):
+    nombre = input("Ingrese el nombre del contacto: ")
+    telefono = input(f"Ingrese el teléfono del contacto {nombre}: ")
+    contactos[nombre] = telefono
+    print(f"Contacto agregado: {nombre} - {telefono}")
+
+print("\nLista de contactos: ")
+lista_contactos = contactos.keys()
+print(lista_contactos)
+print("\nBúsqueda de contactos:")
+nombre_buscar = input("Ingrese el nombre del contacto a buscar: ")
+if nombre_buscar in contactos:
+    print(f"El contacto {nombre_buscar} tiene el numero {contactos[nombre_buscar]}")
+else:
+    print(f"El contacto {nombre_buscar} no está en la lista.")
+
+# ==========================================
+#               EJERCICIO 5
+# ==========================================
