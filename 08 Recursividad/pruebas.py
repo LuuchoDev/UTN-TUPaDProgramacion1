4c355eb65d0b459791ad841583a3373e1b8919e5
LuuchoDev <emanuelsosa00@hotmail.com>
2025-10-25 19:38:00 -0300

    Add: exercise 1 and 2

--- 08 Recursividad/pruebas.py
+++ 08 Recursividad/pruebas.py
@@ -1,11 +1,10 @@
-def factorial_recursivo(x):
-    if x == 1:
+def potencia_recursiva(base, exponente):
+    if exponente == 0:
         return 1
     else:
-        return x * factorial_recursivo(x - 1)
+        return base * potencia_recursiva(base, exponente - 1)
 
-num_usuario = int(input("Ingrese un número para calcular su factorial: "))
-
-for i in range (1, num_usuario + 1):
-    resultado = factorial_recursivo(i)
-    print(f"El factorial de {i} es: {resultado}")
+num_usuario = int(input("Ingrese un número para calcular la potencia: "))
+base = int(input("Ingrese la base: "))
+resultado = potencia_recursiva(base, num_usuario)
+print(f"{base} elevado a la {num_usuario} es: {resultado}")
